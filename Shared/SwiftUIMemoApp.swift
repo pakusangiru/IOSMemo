f2447db49cd5cae760ef91db48b7ba5c334438f9
sangil park <wolf5839@gmail.com>
2023-02-20 19:32:05 +0900

    Initial Commit

---
+++ Shared/SwiftUIMemoApp.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIMemoApp.swift
+//  Shared
+//
+//  Created by park sangil on 2023/02/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMemoApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
